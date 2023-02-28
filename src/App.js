95a3441a7ddfdc85a7234cd856129f6251b56faf
BillStephens2022 <two4onebill@yahoo.com>
2023-02-28 17:33:19 -0500

    added event handlers as props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import Button from "./Button";
+import { GoBell, GoBeaker, GoCloudDownload, GoDatabase, GoJersey } from 'react-icons/go';
 
 function App() {
+
+  const handleClick = () => {
+    console.log('CLICK!');
+  };
+
   return (
     <div>
       <div>
-        <Button success rounded>Click Me!</Button>
+        <Button className="mb-5"onClick={handleClick} primary><GoBell />Click Me!</Button>
       </div>
       <div>
-        <Button danger outline >Buy Now!</Button>
+        <Button onClick={handleClick} secondary><GoBeaker />Buy Now!</Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button onClick={handleClick} success><GoCloudDownload />See Deal!</Button>
       </div>
       <div>
-        <Button secondary>Hide Adds!</Button>
+        <Button onClick={handleClick} warning><GoDatabase />Hide Adds!</Button>
       </div>
       <div>
-        <Button primary rounded>Go Back!</Button>
+        <Button onClick={handleClick} danger><GoJersey />Go Back!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} primary outline >Click Me!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} secondary outline>Buy Now!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} success outline>See Deal!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} warning outline>Hide Adds!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} danger outline>Go Back!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} primary rounded>Click Me!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} secondary rounded>Buy Now!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} success rounded>See Deal!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} warning rounded>Hide Adds!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} danger rounded>Go Back!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} outline rounded primary>Click Me!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} outline rounded secondary>Buy Now!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} rounded outline success>See Deal!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} outline rounded warning>Hide Adds!!</Button>
+      </div>
+      <div>
+        <Button onClick={handleClick} outline rounded danger>Go Back!</Button>
       </div>
     </div>
   );
