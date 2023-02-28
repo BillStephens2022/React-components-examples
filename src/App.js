e280ee2e10934d1dd589cab154a2511ae66b752f
BillStephens2022 <two4onebill@yahoo.com>
2023-02-28 14:32:31 -0500

    added prop-types and tailwindcss packages and installed.  added button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import Button from "./Button";
 
 function App() {
   return (
     <div>
-      App
+      <div>
+        <Button success rounded outline>Click Me!</Button>
+      </div>
+      <div>
+        <Button danger outline >Buy Now!</Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>Hide Adds!</Button>
+      </div>
+      <div>
+        <Button secondary rounded>Go Back!</Button>
+      </div>
     </div>
   );
 }
