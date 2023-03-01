9f8c9469666fbdc8c741cd2ac4c5f3d77cf91ef7
BillStephens2022 <two4onebill@yahoo.com>
2023-03-01 16:25:38 -0500

    added styling and additional functionality to dropdown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import ButtonPage from './pages/ButtonPage';
-import AccordionPage from './pages/AccordionPage';
-import Dropdown from './components/Dropdown';
+import { useState } from "react";
+import ButtonPage from "./pages/ButtonPage";
+import AccordionPage from "./pages/AccordionPage";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   const [selection, setSelection] = useState(null);
@@ -12,37 +12,42 @@ function App() {
 
   const items = [
     {
-      id: 'djfkj2',
-      label: 'Can I use React on a Project?',
-      content: 'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.'
+      id: "djfkj2",
+      label: "Can I use React on a Project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
-      id: 'adjfkj2',
-      label: 'Can I use JavaScript on a Project?',
-      content: 'You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want.'
+      id: "adjfkj2",
+      label: "Can I use JavaScript on a Project?",
+      content:
+        "You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want.",
     },
     {
-      id: 'bdjfkr5',
-      label: 'Can I use CSS on a Project?',
-      content: 'You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.'
-    }
+      id: "bdjfkr5",
+      label: "Can I use CSS on a Project?",
+      content:
+        "You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.",
+    },
   ];
 
   const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' },
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
 
   const handleClick = () => {
-    console.log('CLICK!');
+    console.log("CLICK!");
   };
 
   return (
     <div>
       <ButtonPage onClick={handleClick} />
-      <AccordionPage items={items}/>
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <AccordionPage items={items} />
+      <div className="flex">
+        <Dropdown options={options} value={selection} onChange={handleSelect} />
+      </div>
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      };
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+    
+  }, []);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -15,13 +37,31 @@ function Dropdown({ options, value, onChange }) {
   };
 
   const renderedOptions = options.map((option) => {
-    return <div onClick={() => handleOptionClick(option)} key={option.value}>{option.label}</div>;
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointed p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg"/>
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full">
+          {renderedOptions}
+        </Panel>
+      )}
     </div>
   );
 }
