7cfb4ca3807199ef9d81b52a75e943383c9a13ae
BillStephens2022 <two4onebill@yahoo.com>
2023-03-02 15:17:09 -0500

    added navigation to app

--- src/App.js
+++ src/App.js
@@ -1,52 +1,33 @@
-import { useState } from "react";
-import ButtonPage from "./pages/ButtonPage";
-import AccordionPage from "./pages/AccordionPage";
-import Dropdown from "./components/Dropdown";
+// import { useState } from 'react';
+// import ButtonPage from './pages/ButtonPage';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+import Link from './components/Link';
+import Route from './components/Route';
 
 function App() {
-  const [selection, setSelection] = useState(null);
+  // const [selection, setSelection] = useState(null);
 
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
+  // const handleSelect = (option) => {
+  //   setSelection(option);
+  // };
 
-  const items = [
-    {
-      id: "djfkj2",
-      label: "Can I use React on a Project?",
-      content:
-        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
-    },
-    {
-      id: "adjfkj2",
-      label: "Can I use JavaScript on a Project?",
-      content:
-        "You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want.",
-    },
-    {
-      id: "bdjfkr5",
-      label: "Can I use CSS on a Project?",
-      content:
-        "You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.",
-    },
-  ];
 
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  const handleClick = () => {
-    console.log("CLICK!");
-  };
+  // const handleClick = () => {
+  //   console.log("CLICK!");
+  // };
 
   return (
     <div>
-      <ButtonPage onClick={handleClick} />
-      <AccordionPage items={items} />
-      <div className="flex">
-        <Dropdown options={options} value={selection} onChange={handleSelect} />
+      <Link to="/accordion">Go to accordion</Link>
+      <Link to="/dropdown">Go to dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
       </div>
     </div>
   );
--- src/pages/AccordionPage.js
+++ src/pages/AccordionPage.js
@@ -1,7 +1,27 @@
 
 import Accordion from '../components/Accordion';
 
-function AccordionPage({ items }) {
+function AccordionPage() {
+  const items = [
+    {
+      id: "djfkj2",
+      label: "Can I use React on a Project?",
+      content:
+        "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+    },
+    {
+      id: "adjfkj2",
+      label: "Can I use JavaScript on a Project?",
+      content:
+        "You can use JavaScript on any project you want. You can use JavaScript on any project you want. You can use JavaScript on any project you want.",
+    },
+    {
+      id: "bdjfkr5",
+      label: "Can I use CSS on a Project?",
+      content:
+        "You can use CSS on any project you want. You can use CSS on any project you want. You can use CSS on any project you want.",
+    },
+  ];
 
   return (
     <div>
