a7b71cdf139d503800525de5187005b2bc3d5bbd
BillStephens2022 <two4onebill@yahoo.com>
2023-03-05 16:43:19 -0500

    added sort functionality to table component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -14,7 +14,7 @@ function TablePage() {
     },
     {
       name: "Banana",
-      color: "bg-yellow-500",
+      color: "bg-yellow-300",
       score: 1,
     },
     {
@@ -22,12 +22,28 @@ function TablePage() {
       color: "bg-green-500",
       score: 4,
     },
+    {
+      name: "Cherry",
+      color: "bg-red-700",
+      score: 2,
+    },
   ];
 
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
-    { label: "Color", render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div> },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
   ];
 
   const keyFn = (fruit) => {
@@ -36,7 +52,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn}/>
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
